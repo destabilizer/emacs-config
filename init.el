cb4d2b7acc23e3b004da693d454df1001d415f86
Balaram Usov <dest@disroot.org>
2019-05-23 02:06:32 +0300

    Improved emacs settings using instrunctions from https://habr.com/ru/post/248663/

--- init.el
+++ init.el
@@ -32,12 +32,16 @@
  ;; If there is more than one, they won't work right.
  )
 
+;;; My name and e-mail adress
+(setq user-full-name   "%Balaram Usov%")
+(setq user-mail-adress "%dest@disroot.org%")
 
 ;;; Some preferences
 
 ;; global variables
 (setq
  inhibit-startup-screen t
+ ingibit-startup-message t
  create-lockfiles nil
  make-backup-files nil
  column-number-mode t
@@ -52,9 +56,114 @@
  tab-width 4
  c-basic-offset 4)
 
-;; modes
-(electric-indent-mode 0)
+;; Electric-modes
+(electric-pair-mode    1)
+(electric-indent-mode -1)
 
+;; Delete selection
+(delete-selection-mode t)
+
+;; Dired
+(require 'dired)
+(setq dired-recursive-deletes 'top)
+
+;; Imenu
+(require 'imenu)
+(setq imenu-auto-rescan      t)
+(setq imenu-use-popup-menu nil)
+;(global-set-key (kbd "<f6>") 'imenu)
+
+;; Display the name of the current buffer in the title bar
+(setq frame-title-format "emacs: %b")
+
+;; Org-mode settings
+(require 'org)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key "\C-cl" 'org-store-link)
+(add-to-list 'auto-mode-alist '("\\.org$" . Org-mode))
+
+;; Show-paren-mode settings
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+
+;; Disable GUI components
+(tooltip-mode      -1)
+(menu-bar-mode     -1)
+(tool-bar-mode     -1)
+(scroll-bar-mode   -1)
+(blink-cursor-mode -1)
+(setq use-dialog-box     nil)
+(setq redisplay-dont-pause t)
+(setq ring-bell-function 'ignore)
+
+;; Coding-system settings
+(set-language-environment 'UTF-8)
+(progn
+  (setq default-buffer-file-coding-system 'utf-8)
+  (setq-default coding-system-for-read    'utf-8)
+  (setq file-name-coding-system           'utf-8)
+  (set-selection-coding-system            'utf-8)
+  (set-keyboard-coding-system        'utf-8-unix)
+  (set-terminal-coding-system             'utf-8)
+  (prefer-coding-system                   'utf-8)))
+
+;; Font
+(add-to-list 'default-frame-alist '(font . "Hack-11" ))
+(set-face-attribute 'default t :font "Hack-11" )
+
+;; Linum plugin
+(require 'linum)
+(line-number-mode   t)
+(global-linum-mode  t)
+(column-number-mode t)
+(setq linum-format " %d")
+
+;; Fringe settings
+(fringe-mode '(8 . 0)) ;; органичиталь текста только слева
+(setq-default indicate-empty-lines t) ;; отсутствие строки выделить глифами рядом с полосой с номером строки
+(setq-default indicate-buffer-boundaries 'left) ;; индикация только слева
+
+;; Display file size/time in mode-line
+(setq display-time-24hr-format t) ;; 24-часовой временной формат в mode-line
+(display-time-mode             t) ;; показывать часы в mode-line
+(size-indication-mode          t) ;; размер файла в %-ах
+
+;; Line wrapping
+(setq word-wrap          t) ;; переносить по словам
+(global-visual-line-mode t)
+
+;; IDO plugin
+(require 'ido)
+(ido-mode                      t)
+(icomplete-mode                t)
+(ido-everywhere                t)
+(setq ido-vitrual-buffers      t)
+(setq ido-enable-flex-matching t)
+
+;; Buffer Selection and ibuffer settings
+(require 'bs)
+(require 'ibuffer)
+(defalias 'list-buffers 'ibuffer) ;; отдельный список буферов при нажатии C-x C-b
+(global-set-key (kbd "<f2>") 'bs-show) ;; запуск buffer selection кнопкой F2
+
+;; Easy transition between buffers: M-arrow-keys
+(if (equal nil (equal major-mode 'org-mode))
+  (windmove-default-keybindings 'meta))
+
+;; Highlight search resaults
+(setq search-highlight        t)
+(setq query-replace-highlight t)
+
+;; Bookmark settings
+(require 'bookmark)
+(setq bookmark-save-flag t) ;; автоматически сохранять закладки в файл
+(when (file-exists-p (concat user-emacs-directory "bookmarks"))
+    (bookmark-load bookmark-default-file t)) ;; попытаться найти и открыть файл с закладками
+(global-set-key (kbd "<f3>") 'bookmark-set) ;; создать закладку по F3 
+(global-set-key (kbd "<f4>") 'bookmark-jump) ;; прыгнуть на закладку по F4
+(global-set-key (kbd "<f5>") 'bookmark-bmenu-list) ;; открыть список закладок
+(setq bookmark-default-file (concat user-emacs-directory "bookmarks")) ;; хранить закладки в файл bookmarks в .emacs.d
 
 ;;; Extensions
 
@@ -62,13 +171,7 @@
 (add-to-list 'load-path extension-path)
 
 ;; Theme
-
 (load-theme 'base16-atelier-forest t)
-
-;; Font
-
-(add-to-list 'default-frame-alist '(font . "Hack-11" ))
-(set-face-attribute 'default t :font "Hack-11" )
 
 ;; Smooth scrolling
 (require 'smooth-scroll)
@@ -82,14 +185,12 @@
 
 
 ;; (defcustom smooth-scroll/
-
 (global-set-key [(control  down)]  'scroll-up-1)
 (global-set-key [(control  up)]    'scroll-down-1)
 (global-set-key [(control  left)]  'scroll-right-1)
 (global-set-key [(control  right)] 'scroll-left-1)
 
 ;; Sublimity
-
 (add-to-list 'load-path (concat extension-path "sublimity/"))
 
 (require 'sublimity)
@@ -98,11 +199,9 @@
 ;(require 'sublimity-attractive)
 
 ;; 
-
 (sublimity-mode 1)
 
 ;; Android Mode
-
 ; (require 'android-mode)
 
 ;; SuperCollider
